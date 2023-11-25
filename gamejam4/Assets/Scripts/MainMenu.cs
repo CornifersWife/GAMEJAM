6cd02b37d665247c40e140e27bd146dabf8ad223
s22714 <s22714@pjwstk.edu.pl>
2023-11-25 17:33:13 +0100

    Main menu

---
+++ gamejam4/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainView;
+    [SerializeField] private GameObject _settingsView;
+    [SerializeField] private GameObject _levelSelectionView;
+
+    private void Awake()
+    {
+        _mainView.SetActive(true);
+        _settingsView.SetActive(false);
+        _levelSelectionView.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        _mainView.SetActive(false);
+        _levelSelectionView.SetActive(true);
+        _settingsView.SetActive(false);
+    }
+
+    public void LevelSelect(int levelID)
+    {
+        SceneManager.LoadScene(levelID);
+    }
+
+    public void ReturnToMain()
+    {
+        _mainView.SetActive(true);
+        _levelSelectionView.SetActive(false);
+        _settingsView.SetActive(false);
+    }
+}
