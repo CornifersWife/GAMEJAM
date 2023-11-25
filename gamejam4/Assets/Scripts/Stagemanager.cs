7c2dbb682ec31627f9247fe6e2e630cc3c559a76
CornifersWife <irys.latosz@gmail.com>
2023-11-25 21:14:39 +0100

    im done

--- gamejam4/Assets/Scripts/Stagemanager.cs
+++ gamejam4/Assets/Scripts/Stagemanager.cs
@@ -1,16 +1,17 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Stagemanager : MonoBehaviour {
     [SerializeField] private float first_wave_time = 5f;
     [SerializeField] private float spawn_interval = 15f;
-    [SerializeField] private GameObject targetObject;
+    public List<GameObject> objectList;
     [SerializeField] private int enemies_to_spawn = 5; 
 
     private void Start() {
         InvokeRepeating("StartRound", first_wave_time, spawn_interval);
     }
-
+   
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             CancelInvoke("StartRound");
@@ -21,8 +22,8 @@ public class Stagemanager : MonoBehaviour {
 
     // ReSharper disable Unity.PerformanceAnalysis
     private void StartRound() {
-        if (targetObject != null) {
-            targetObject.BroadcastMessage("Start_spawning", enemies_to_spawn, SendMessageOptions.DontRequireReceiver);
+        for (int i = 0; i < objectList.Count; i++) {
+            objectList[i].BroadcastMessage("Start_spawning",enemies_to_spawn, SendMessageOptions.DontRequireReceiver);
         }
     }
 }
