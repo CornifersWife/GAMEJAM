2fc00053c8d64f28d9962212c417da41c15c91bf
CornifersWife <irys.latosz@gmail.com>
2023-11-25 16:56:47 +0100

    fixed bugs

--- gamejam4/Assets/Scripts/enemy_spawn.cs
+++ gamejam4/Assets/Scripts/enemy_spawn.cs
@@ -2,18 +2,13 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class enemy_spawn : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class enemy_spawn : MonoBehaviour {
+
     [SerializeField] private GameObject _child;
-    [SerializeField] private Boolean spawn= false; //may be used for animating "opening it"
+    [SerializeField] private Boolean spawn = false; 
     [SerializeField] private float min_spawn_delay = 1.5f;
-    [SerializeField] private int enemies_to_spawn = 5; //used for testing
     [SerializeField] private float spawn_delay_random_max = 0.3f;
-    //private Collider2D _c2d;
-    private void Awake() {
-        //_c2d = GetComponent<Collider2D>();
-    }
+
 
     private void OnTriggerEnter2D(Collider2D other) {
         spawn = false;
@@ -23,27 +18,21 @@ public class enemy_spawn : MonoBehaviour
         spawn = true;
     }
 
-    private void Update() {
-        if (spawn) {
-            Start_spawning(enemies_to_spawn);
-        }
+    public void Start_spawning(int amount) {
+        StartCoroutine(Spawn_enemy_Coroutine(amount));
     }
 
-    public void Start_spawning(int amount) {
-        var xd =Spawn_enemy_Coroutine(amount);
-    }
     IEnumerator Spawn_enemy_Coroutine(int amount) {
-        yield return new WaitForSeconds(min_spawn_delay);
+        
         spawn = true;
         for (var i = 0; i < amount;) {
             var randDelay = UnityEngine.Random.Range(0f, spawn_delay_random_max);
             yield return new WaitForSeconds(min_spawn_delay + randDelay);
-            //if (spawn) {
-            Instantiate(_child, transform.position, transform.rotation);
+            if (spawn) {
+                Instantiate(_child, transform.position, transform.rotation);
                 i++;
-           // }
+            }
         }
-
         spawn = false;
     }
 }
