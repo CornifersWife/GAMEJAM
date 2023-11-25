c51ca45a724de7f6afce99623a5a69f2f9af4ddf
Ciri-Zireael <ewajastrzebska21@gmail.com>
2023-11-25 15:30:54 +0100

    Added skeleton for the weapon script

---
+++ gamejam4/Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float cooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float damage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
