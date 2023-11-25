050f5821a0f015d07d78349207f6bad879c782c6
s22714 <s22714@pjwstk.edu.pl>
2023-11-25 19:30:24 +0100

    enemy animations controller script

--- gamejam4/Assets/Scripts/enemy_movement.cs
+++ gamejam4/Assets/Scripts/enemy_movement.cs
@@ -7,8 +7,11 @@ public class Chaser : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;  // The speed at which the object will move
     private Rigidbody2D _rb;
 
+    private EnemyAnimations _enemyAnimations; // animations controller script
+
     private void Awake() {
         _rb = GetComponent<Rigidbody2D>();
+        _enemyAnimations = GetComponent<EnemyAnimations>();
     }
 
     void Update()
@@ -20,7 +23,10 @@ public class Chaser : MonoBehaviour
             Vector2 direction = (target.position - transform.position).normalized;
             _rb.velocity = direction * moveSpeed;
             //_rb.transform.Translate(e);
+
             
         }
+        
     }
+
 }
