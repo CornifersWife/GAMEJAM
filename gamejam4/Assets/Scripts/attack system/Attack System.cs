e93667830ad8f766b6b9d80f43abffc5bfbbbc8c
Zahar Zubik <zaharzubik@gmail.com>
2023-11-25 16:10:09 +0100

    start attack system

---
+++ gamejam4/Assets/Scripts/attack system/Attack System.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSystem : MonoBehaviour
+{
+    [SerializeField] private Transform WeaponSlot1;
+    [SerializeField] private Transform WeaponSlot2;
+    [SerializeField] private Transform WeaponSlot3;
+    [SerializeField] private Transform WeaponSlot4;
+
+void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
