77026d7872da06e4a925350c902d0978af6a7665
Ciri-Zireael <ewajastrzebska21@gmail.com>
2023-11-25 19:11:13 +0100

    added modifier for weapons
    will be used for instantiating different weapons and modifying the existing ones

---
+++ gamejam4/Assets/Scripts/WeaponModifier.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponModifier : MonoBehaviour
+{
+    [SerializeField] private float speedMod;
+    [SerializeField] private float cooldownMod;
+    [SerializeField] private float rangeMod;
+    [SerializeField] private float damageMod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
