ae4fb0e51acb7cc614340498a3dc9f17810b3ffb
Ciri-Zireael <ewajastrzebska21@gmail.com>
2023-11-25 18:46:54 +0100

    added prefab for modifiers (buffs/debuffs)

---
+++ gamejam4/Assets/Scripts/PlayerStatModifier.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatModifier : MonoBehaviour
+{
+    [SerializeField] private float timeDuration;
+    [SerializeField] private float speedMovementMod;
+    [SerializeField] private float speedAttackMod;
+    [SerializeField] private float damageMod;
+    [SerializeField] private float HPMod;
+    [SerializeField] private float armorMod;
+    [SerializeField] private float dodgeMod;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
