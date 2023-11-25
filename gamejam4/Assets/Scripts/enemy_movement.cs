a334a5bd3a3f8df46d98ad088af6698844ddbe33
CornifersWife <irys.latosz@gmail.com>
2023-11-25 22:33:39 +0100

    changed enemy_movement

--- gamejam4/Assets/Scripts/enemy_movement.cs
+++ gamejam4/Assets/Scripts/enemy_movement.cs
@@ -1,11 +1,11 @@
-
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Chaser : MonoBehaviour
-{
-    private GameObject target;  // The target object to chase
-    [SerializeField] private float moveSpeed = 5f;  // The speed at which the object will move
+public class Chaser : MonoBehaviour {
+    private GameObject target; // The target object to chase
+    [SerializeField] private float moveSpeed = 5f; // The speed at which the object will move
+    [SerializeField] private float distance_from_player = 2f;
     private Rigidbody2D _rb;
 
     private EnemyAnimations _enemyAnimations; // animations controller script
@@ -16,21 +16,16 @@ public class Chaser : MonoBehaviour
         target = GameObject.Find("Player_movement");
 
         _enemyAnimations = GetComponent<EnemyAnimations>();
-
     }
 
-    void Update()
-    {
+    void Update() {
         if (target != null) {
-            
             // Calculate the direction to move towards the target
-            Vector2 direction = (target.transform.position - transform.position).normalized;
-            _rb.velocity = direction * moveSpeed;
+            if ((target.transform.position - transform.position).magnitude > distance_from_player) {
+                Vector2 direction = (target.transform.position - transform.position).normalized;
+                _rb.velocity = direction * moveSpeed;
+            }
             //_rb.transform.Translate(e);
-
-            
         }
-        
     }
-
 }
