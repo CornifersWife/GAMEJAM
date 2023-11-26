4d409950f22dbc987850a3b4ff31ecf0bdc20503
s22714 <s22714@pjwstk.edu.pl>
2023-11-26 03:36:39 +0100

    menu background + some icons

--- gamejam4/Assets/Scripts/UI/Shop.cs
+++ gamejam4/Assets/Scripts/UI/Shop.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     // weapons levels
-    private float _meleeWeaponLevel = 1f;
-    private float _MidRangeWeaponLevel = 1f;
-    private float _FarRangeWeaponLevel = 1f;
+    //private float _meleeWeaponLevel = 1f;
+    //private float _MidRangeWeaponLevel = 1f;
+    //private float _FarRangeWeaponLevel = 1f;
 
     // stats levels
     private float _HP = 1f;
-    private float _HP_Regen = 1f;
+    //private float _HP_Regen = 1f;
     private float _Armor = 1f;
     private float _Movement_Speed = 1f;
     private float _DMG = 1f;
     private float _Attack_Speed = 1f;
-    private float _Crit_Chance = 1f;
-    private float _Crit_Multip = 1f;
+    //private float _Crit_Chance = 1f;
+    //private float _Crit_Multip = 1f;
 
     //text prompts
     [SerializeField] private TMP_Text _meleePrompt;
